bec0ecb3990e5ce7391127333d1bb8298509982f
KbTheBest181 <73401085+KbTheBest181@users.noreply.github.com>
2021-08-04 17:41:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     <script>
     // Set the date we're counting down to
-    var countDownDate = new Date("Aug 4, 2021 17:40:00").getTime();
+    var countDownDate = new Date().getTime() + 1000 * 60 * 1;
 
     // Update the count down every 1 second
     var x = setInterval(function() {
