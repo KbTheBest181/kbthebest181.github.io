92d66f35f1ec4556119ef79851555805ed76d9e3
KbTheBest181 <73401085+KbTheBest181@users.noreply.github.com>
2021-08-04 17:38:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,38 @@
 </head>
 <body>
     <h1>hahahahh</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <!-- Display the countdown timer in an element -->
+    <p id="demo"></p>
+
+    <script>
+    // Set the date we're counting down to
+    var countDownDate = new Date("Aug 4, 2021 17:40:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Hehu";
+      }
+    }, 1000);
+    </script>
 </body>
 </html>
